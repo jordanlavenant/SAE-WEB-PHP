d3a68b5abce9ec5d5782b3b7e1e075944ef6cbde
Jordan <jordan.lavenant@etu.univ-orleans.fr>
2024-02-04 01:54:18 +0100

    Update du aside

--- Classes/AsideHome/Aside.php
+++ Classes/AsideHome/Aside.php
@@ -11,11 +11,14 @@ class Aside {
     function buildAside() {
         return sprintf(
             '<aside>
-                <ul class="menu">
-                    <li><a href="index.php?action=home">accueil</a></li>
-                    <li><a href="index.php?action=bibliotheque">bibliothèque</a></li>
-                    <li><a href="index.php?action=favoris">favoris</a></li>
-                </ul>
+                <section class="header">
+                    <a href="index.php?action=home"><img src="static/images/VisualStudioMusicLogo.png" alt="logo"></a>
+                    <ul class="menu">
+                        <li><a href="index.php?action=home">accueil</a></li>
+                        <li><a href="index.php?action=bibliotheque">bibliothèque</a></li>
+                        <li><a href="index.php?action=favoris">favoris</a></li>
+                    </ul>
+                </section>
                 <a href="index.php?action=login">se déconnecter</a>
             </aside>',
         );
