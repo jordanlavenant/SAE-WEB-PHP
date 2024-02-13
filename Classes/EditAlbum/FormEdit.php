070efda69a2a5bf37042e879804fac7f4403d6c3
Anna Lallier <anlasco231@gmail.com>
2024-02-13 15:26:27 +0100

    Modifier album avec un popup !

--- Classes/EditAlbum/FormEdit.php
+++ Classes/EditAlbum/FormEdit.php
@@ -13,13 +13,38 @@ class FormEdit {
 
     function render(): string {
         return sprintf("
+        
+            <script>
+                function togglePopup(){
+                    let popup = document.querySelector('#popup-overlay');
+                    popup.classList.toggle('open');
+                }
+
+                
+            </script>
+
+            <div id='popup-overlay' class=''>
+                <div class='popup-content'>
+                    <h2>voulez-vous vraiment modifier %s ?</h2>
+
+                    <svg href='javascript:void(0)' onclick='togglePopup()' class='popup-exit' xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 24 24' style='fill: rgba(0, 102, 255, 1);transform: ;msFilter:;'><path d='m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z'></path></svg>
+                    <div class='choices'>
+                        <input type='submit' form='myform' value='modifier album'>
+                        <a class='genericButton' onclick='togglePopup()'>annuler</a>
+                    </div>
+                </div>
+            </div>
+
+
+
+        
             <div class='header'>
                 <a href='index.php?action=home'>
                     <svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 24 24' style='fill: rgba(222, 238, 237, 1);transform: ;msFilter:;'><path d='M13.939 4.939 6.879 12l7.06 7.061 2.122-2.122L11.121 12l4.94-4.939z'></path></svg>
                 </a>
                 <h1>détail</h1>
             </div>
-            <form action='index.php?action=modifierAlbum&id=%s' method='post'>
+            <form id='myform'  action='index.php?action=modifierAlbum&id=%s' method='post'>
                 <section class='album-container'>
                     <div class='content'>
                         <div class='left-part'>
@@ -33,12 +58,13 @@ class FormEdit {
                             </div>
                         </div>
                         <div class='buttons'>
-                            <input type='submit' value='modifier l'album'>
+                            <a class='genericButton' onclick='togglePopup()'>supprimer</a>
                         </div>
                     </div>
                 </section>
             </form>
         </main>",
+            $this->singleData->getTitle(),
             $this->singleData->getEntryId(),
             $this->singleData->getImg(),
             $this->singleData->getTitle(),
