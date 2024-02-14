fa076976b1bd47368fd04d7e8b546d71cc44cefc
Anna Lallier <anlasco231@gmail.com>
2024-02-14 17:29:20 +0100

    Ajout du bouton supprimer

--- Classes/SingleAlbum/Details.php
+++ Classes/SingleAlbum/Details.php
@@ -63,8 +63,12 @@ class Details {
                     </div>
                     <div class='buttons'>
                         <div>
-                            <p>écouter</p>
-                            <svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 24 24' style='fill: rgba(0,102,255,1);transform: ;msFilter:;'><path d='M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z'></path><path d='m9 17 8-5-8-5z'></path></svg>
+                            <a href='index.php?action=delete&id=%s'>Supprimer l'album </a>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 24 24' style='fill: rgba(0, 102, 255, 1);transform: ;msFilter:;'><path d='M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z'></path><path d='M9 10h2v8H9zm4 0h2v8h-2z'></path></svg>
+                        </div>
+                        <div>
+                            <a href='index.php?action=edit&id=%s'>Modifier les informations de l'album </a>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 24 24' style='fill: rgba(0, 102, 255, 1);transform: ;msFilter:;'><path d='M19.045 7.401c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.378-.378-.88-.586-1.414-.586s-1.036.208-1.413.585L4 13.585V18h4.413L19.045 7.401zm-3-3 1.587 1.585-1.59 1.584-1.586-1.585 1.589-1.584zM6 16v-1.585l7.04-7.018 1.586 1.586L7.587 16H6zm-2 4h16v2H4z'></path></svg>
                         </div>
                         <div>
                             <p>ajouter à la bibliothèque</p>
@@ -76,7 +80,7 @@ class Details {
                                 %s
                             </a>
                         </div>
-                    </div>
+                     </div>
                 </div>
             </section>",
             $playlistContent,
@@ -87,6 +91,9 @@ class Details {
             $this->singleData->getNomGroupe(),
             $this->singleData->getGenreString(),
             $this->singleData->getReleaseYear(),
+            $this->singleData->getEntryId(),
+            $this->singleData->getEntryId(),
+            
             $this->liked ? "retirer des favoris" : "ajouter aux favoris",
             $this->singleData->getEntryId(),
             $this->liked 
