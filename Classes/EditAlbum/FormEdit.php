75c8b3ae2cf94460e1b9bbe092359fc81098d5e6
AnnaLallier <144127706+AnnaLallier@users.noreply.github.com>
2024-02-16 00:02:18 +0100

    On peut charger une image sur la page

--- Classes/EditAlbum/FormEdit.php
+++ Classes/EditAlbum/FormEdit.php
@@ -13,39 +13,31 @@ class FormEdit {
 
     function render(): string {
         return sprintf("
-        
-            <script>
-                function togglePopup(){
-                    let popup = document.querySelector('#popup-overlay');
-                    popup.classList.toggle('open');
-                }
+        <script>
+            function togglePopup() {
+                let popup = document.querySelector('#popup-overlay');
+                popup.classList.toggle('open');
+            }
 
-                function handleImageUpload(event) {
-                    const file = event.target.files[0];
-                    const reader = new FileReader();
-                    reader.onload = function(e) {
-                        const imgElement = document.querySelector('#album-image');
-                        imgElement.src = e.target.result;
-                    }
-                    reader.readAsDataURL(file);
-                }
+            function chargeImage(event) {
+                let input = event.target;
+                let reader = new FileReader();
 
-                function updateAlbumImage() {
-                    const imgElement = document.querySelector('#album-image');
-                    const newImageSrc = imgElement.src;
-                    // Call an API or perform any necessary action to update the album image
-                    // Redirect to the album details page
-                    window.location.href = 'index.php?action=album&id=' + <?php echo $this->singleData->getEntryId(); ?>;
-                }
-            </script>
+                reader.onload = function(){
+                    let newAlbumImage = document.getElementById('new-album-image');
+                    newAlbumImage.src = reader.result;
+                };
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        </script>
 
             <div id='popup-overlay' class=''>
                 <div class='popup-content'>
                     <h2>voulez-vous vraiment modifier les informations de cet album ?</h2>
-
                     <svg href='javascript:void(0)' onclick='togglePopup()' class='popup-exit' xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 24 24' style='fill: rgba(0, 102, 255, 1);transform: ;msFilter:;'><path d='m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z'></path></svg>
                     <div class='choices'>
-                        <input type='submit' form='myform' value=Confirmer>
+                        <input type='submit' form='myform' value='Confirmer'>
                         <a class='genericButton' onclick='togglePopup()'>Annuler</a>
                     </div>
                 </div>
@@ -58,11 +50,12 @@ class FormEdit {
                 <h1>détail</h1>
             </div>
             
-            <form class= 'myform' id='myform'  action='index.php?action=modifierAlbum&id=%s' method='post'>
+            <form class='myform' id='myform' action='index.php?action=modifierAlbum&id=%s' method='post'>
                 <section class='album-container'>
                     <div class='content'>
                         <div class='left-part'>
                             <img id='album-image' src='%s' alt='%s'>
+                            <img id='new-album-image' src='' alt='New Album Image'>
                             <div class='album-info'>
                                 <label for='title'>Titre</label>
                                 <input type='text' name='title' value='%s'>
@@ -79,8 +72,7 @@ class FormEdit {
                         <div class='buttons'>
                             <a class='genericButton' onclick='togglePopup()'>Modifier l'album</a>
                             <label for='image-upload' class='genericButton'>Modifier l'image</label>
-                            <input id='image-upload' type='file' accept='image/*' style='display: none;' onchange='handleImageUpload(event)'>
-                            <a class='genericButton' onclick='updateAlbumImage()'>Valider</a>
+                            <input id='image-upload' type='file' accept='image/*' style='display: none;' onchange='chargeImage(event)'>
                         </div>
                     </div>
                 </section>
