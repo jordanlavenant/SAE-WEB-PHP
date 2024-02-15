b872e3db39d0a480d828f6a7af32783a68e7c365
jordanlavenant <jordan.lavenant@etu.univ-orleans.fr>
2024-02-15 08:17:38 +0100

    Modification formulaire d'importation des données

--- Classes/ImportData/ImportDataForm.php
+++ Classes/ImportData/ImportDataForm.php
@@ -9,14 +9,14 @@ class ImportDataForm {
 
     function render() {
         return sprintf(
-            '<form class="importDataForm" action="index.php?action=ajouterAlbum" method="post">
+            '<form id="importDataForm" class="importDataForm" action="index.php?action=ajouterAlbum" method="post">
                 <input required type="text" name="by" placeholder="groupe">
                 <input required type="text" name="title" placeholder="titre">
                 <input required type="text" name="parent" placeholder="artiste, compositeur">
                 <input required type="text" name="releaseYear" placeholder="date de sortie">
                 <input type="text" name="img" placeholder="image">
-                <input type="submit" value="ajouter">
-            </form>'
+            </form>
+            <div class="importDataFormSubmit"><input type="submit" form="importDataForm" value="ajouter"></div>'
         );
     }
 }
