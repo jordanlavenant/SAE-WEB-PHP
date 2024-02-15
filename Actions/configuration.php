25106206c71ae12a8c9eb7d11b8cbecc994a0f1f
jordanlavenant <jordan.lavenant@etu.univ-orleans.fr>
2024-02-15 13:58:25 +0100

    Configuration des radios buttons

--- Actions/configuration.php
+++ Actions/configuration.php
@@ -31,6 +31,20 @@
         
             echo "
                 <main>
+
+                    <script>
+                        function changeTheme(element) {
+                            element.form.submit();
+                            let label = element.nextElementSibling;
+                            console.log(label);
+                            if (element.checked) {
+                                label.style.tetxDecoration = 'underline';
+                            } else {
+                                label.style.tetxDecoration = 'none';
+                            }
+                        }
+                    </script>
+
                     <div class='header'>
                         <a href='index.php?action=home'>
                             <svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 24 24' style='fill: rgba(222, 238, 237, 1);transform: ;msFilter:;'><path d='M13.939 4.939 6.879 12l7.06 7.061 2.122-2.122L11.121 12l4.94-4.939z'></path></svg>
@@ -38,21 +52,25 @@
                         <h1>thème</h1>
                     </div>
                     <form class='myform-theme' method='post'>
-                        <h2>light</h2>";
+                        <h2>light</h2>
+                        <div class='themeSelector'>";
                         foreach ($this->themesLight as $theme) {
                             $checked = $theme === $this->currentTheme ? 'checked' : '';
                             echo "
-                            <input type='radio' id='$theme' name='theme' value='$theme' $checked onchange='this.form.submit()'>
+                            <input type='radio' id='$theme' name='theme' value='$theme' $checked onchange='changeTheme(this)'>
                             <label for='$theme'>$theme</label>";
                         }
+                        echo "</div>";
                         echo "
-                            <h2>dark</h2>";
-                            foreach ($this->themesDark as $theme) {
-                                $checked = $theme === $this->currentTheme ? 'checked' : '';
-                                echo "
-                                <input type='radio' id='$theme' name='theme' value='$theme' $checked onchange='this.form.submit()'>
-                                <label  for='$theme'>$theme</label>";
-                            }
+                        <h2>dark</h2>
+                        <div class='themeSelector'>";
+                        foreach ($this->themesDark as $theme) {
+                            $checked = $theme === $this->currentTheme ? 'checked' : '';
+                            echo "
+                            <input type='radio' id='$theme' name='theme' value='$theme' $checked onchange='changeTheme(this)'>
+                            <label for='$theme'>$theme</label>";
+                        }
+                        echo "</div>";
                         echo "  
                         </form>
                         </body>
