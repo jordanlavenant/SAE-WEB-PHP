e3f2aa9a01c3838ebce4d4ab61164c6419637100
AnnaLallier <144127706+AnnaLallier@users.noreply.github.com>
2024-02-17 20:39:26 +0100

    Suppression d'un groupe, modification d'un groupe

---
+++ Actions/groupe.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+require_once('BD/getBd.php');
+
+
+require 'Classes/AsideHome/Aside.php';
+require 'Classes/Groupes/DisplayGroupe.php';
+
+use AsideHome\Aside;
+use Groupe\DisplayGroupe;
+
+class Groupe {
+
+    private string $artist;
+    private array $artistData;
+
+    function __construct(string $artist) {
+        $this->artist = $artist;
+    }
+
+    function buildGroupe(): void {
+
+        $aside = new Aside();
+        echo $aside->buildAside();
+
+        echo "
+        <main>
+            <div class='header'>
+                <a href='index.php?action=groupes'>
+                    <svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 24 24' style='fill: rgba(222, 238, 237, 1);transform: ;msFilter:;'><path d='M13.939 4.939 6.879 12l7.06 7.061 2.122-2.122L11.121 12l4.94-4.939z'></path></svg>
+                </a>
+                <h1>".$this->artist."</h1>
+            </div>";
+            $displayGroupe = new DisplayGroupe($this->artist);
+            $displayGroupe->render();
+        echo "
+        </main>";
+    }
+}
