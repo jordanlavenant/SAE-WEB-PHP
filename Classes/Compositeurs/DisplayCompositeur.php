4923a68831d70d67337c6c5567c5dd4289b2fd3f
AnnaLallier <144127706+AnnaLallier@users.noreply.github.com>
2024-02-17 20:42:18 +0100

    Suppression d'un groupe, modification d'un groupe

--- Classes/Compositeurs/DisplayCompositeur.php
+++ Classes/Compositeurs/DisplayCompositeur.php
@@ -40,7 +40,7 @@ class DisplayCompositeur {
     function render(): void {
         $displayAlbums = new DisplayFilteteredAlbums($this->artistData);
         echo "
-        <script>
+            <script>
                 function togglePopupDelete(){
                     let popup = document.querySelector('#popup-overlay-delete');
                     popup.classList.toggle('open');
@@ -63,6 +63,7 @@ class DisplayCompositeur {
             <a onclick='togglePopupDelete()'>
                 <svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 24 24' style='fill:" . $_SESSION['hexa'] . " ;transform: ;msFilter:;'><path d='M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z'></path><path d='M9 10h2v8H9zm4 0h2v8h-2z'></path></svg>
             </a>
+            
             <div id='compositeur-props'>";
                 $size = count($this->artistData);
                 if ($size > 1) echo "<h3>Ce compositeur a contribué à <span>".$size." </span>albums</h3>";
