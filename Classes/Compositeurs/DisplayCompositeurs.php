a78460e6861dd5aa97188e70fb474d59c27dc158
jordanlavenant <jordan.lavenant@etu.univ-orleans.fr>
2024-02-17 13:44:18 +0100

    Initialisation des compositeurs

--- Classes/Compositeurs/DisplayCompositeurs.php
+++ Classes/Compositeurs/DisplayCompositeurs.php
@@ -14,17 +14,23 @@ class DisplayCompositeurs {
     function __construct(array $data) {
         $this->artists = getAllArtist();    
         $this->artistData = array();
-        foreach($this->artists as $artist) {
-            array_push($this->artistData, getAlbumByArtist($artist));
-        }
+        // foreach($this->artists as $artist) {
+        //     array_push($this->artistData, getAlbumByArtist($artist));
+        // }
     }
 
     function render(): void {
-        $displayArtists = new DisplayFilteteredAlbums($this->artistData);
-        echo "<h2>discographie compositeur</h2>";
-        echo "<section class='discographie'>";
-            $displayAlbums->buildAlbums();
-        echo "</section>";
+        echo "
+        <div class='header'>
+            <a href='index.php?action=home'>
+                <svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 24 24' style='fill: rgba(222, 238, 237, 1);transform: ;msFilter:;'><path d='M13.939 4.939 6.879 12l7.06 7.061 2.122-2.122L11.121 12l4.94-4.939z'></path></svg>
+            </a>
+            <h1>compositeurs</h1>
+        </div>
+        <section class='compositeurs'>
+            
+        </section>
+        ";
+        
     }
 }
-
