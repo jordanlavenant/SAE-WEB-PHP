3d400e08048b07fcb88640dd6074252770832aa8
AnnaLallier <144127706+AnnaLallier@users.noreply.github.com>
2024-02-17 17:11:03 +0100

    Suppression de compositeur

--- Classes/Compositeurs/DisplayCompositeur.php
+++ Classes/Compositeurs/DisplayCompositeur.php
@@ -18,8 +18,10 @@ use AllAlbum\Album;
 class DisplayCompositeur {
 
     private array $artistData;
+    private string $parent;
 
     function __construct(string $parent) {    
+        $this->parent = $parent;
         $this->artistData = getAlbumWithParent($parent);
         // Transformer les données en objet Album
         $this->artistData = array_map(function($content) {
@@ -37,14 +39,36 @@ class DisplayCompositeur {
 
     function render(): void {
         $displayAlbums = new DisplayFilteteredAlbums($this->artistData);
-        echo "<section class='compositeur-album'>
+        echo "
+        <script>
+                function togglePopupDelete(){
+                    let popup = document.querySelector('#popup-overlay-delete');
+                    popup.classList.toggle('open');
+                }  
+            </script>
+
+            <div id='popup-overlay-delete' class=''>
+                <div class='popup-content'>
+                    <h2>voulez-vous vraiment supprimer ce compositeur, ainsi que tous les albums qui lui sont associés ?</h2>
+
+                    <svg href='javascript:void(0)' onclick='togglePopupDelete()' class='popup-exit' xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 24 24' style='fill:" . $_SESSION['hexa'] . " ;transform: ;msFilter:;'><path d='m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z'></path></svg>
+                    <div class='choices'>
+                        <a class='genericButton' href='index.php?action=supprimerCompositeur&parent=" . $this->parent .  "'>Confirmer</a>
+                        <a class='genericButton' onclick='togglePopupDelete()'>Annuler</a>
+                    </div>
+                </div>
+            </div>
+        </script>
+        <section class='compositeur-album'>
+            <a onclick='togglePopupDelete()'>
+                <svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' viewBox='0 0 24 24' style='fill:" . $_SESSION['hexa'] . " ;transform: ;msFilter:;'><path d='M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z'></path><path d='M9 10h2v8H9zm4 0h2v8h-2z'></path></svg>
+            </a>
             <div id='compositeur-props'>";
                 $size = count($this->artistData);
-                if ($size > 1) echo "<h3>ce compositeur a contribué à <span>".$size." </span>albums</h3>";
-                else echo "<h3>ce compositeur a contribué à <span>".$size." </span>album</h3>"; 
+                if ($size > 1) echo "<h3>Ce compositeur a contribué à <span>".$size." </span>albums</h3>";
+                else echo "<h3>Ce compositeur a contribué à <span>".$size." </span>album</h3>"; 
             echo "</div>";
             $displayAlbums->buildAlbums();
         echo "</section>";
     }
 }
-
