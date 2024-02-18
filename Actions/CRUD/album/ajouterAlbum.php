fd78f5f6c78be8aaedb89b60417079cc68531bc0
AnnaLallier <144127706+AnnaLallier@users.noreply.github.com>
2024-02-18 14:20:58 +0100

    Correction de la redirection lors de l'ajout d'un album & affichage de l'image

--- Actions/CRUD/album/ajouterAlbum.php
+++ Actions/CRUD/album/ajouterAlbum.php
@@ -8,16 +8,18 @@ $by = $_POST['by'];
 $title = $_POST['title'];
 $parent = $_POST['parent'];
 $releaseYear = $_POST['releaseYear'];
+$insertedAlbumId = getLastEntryId() + 1;
 
 if (isset($_FILES["img"]) && $_FILES["img"]["error"] == UPLOAD_ERR_OK) {
     $image_name = $_FILES["img"]["name"];
     $image_temp = $_FILES["img"]["tmp_name"];
     $target_path = "data/images/" . $image_name;
+    
 
     move_uploaded_file($image_temp, $target_path);
-    insererAlbum($by, getLastEntryId() + 1, array(), $image_name, $parent, $releaseYear, $title);
+    insererAlbum($by, $insertedAlbumId, array(), $image_name, $parent, $releaseYear, $title);
 } else {
-    insererAlbum($by, getLastEntryId() + 1, array(), "null", $parent, $releaseYear, $title);
+    insererAlbum($by, $insertedAlbumId, array(), "null", $parent, $releaseYear, $title);
 }
 
-header("Location: index.php?action=import");        
+header("Location: index.php?action=album&id=" . $insertedAlbumId);        
